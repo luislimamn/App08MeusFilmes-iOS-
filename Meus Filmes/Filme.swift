a549faf1879208750aa0406b5308c189494ccfb9
Luis Andre de Lima <luislimamn@gmail.com>
2019-06-14 12:27:16 -0300

    Versão 1.0
    Lista de Filmes e Descrição, Otimizado para iPhone 8

---
+++ Meus Filmes/Filme.swift
@@ -0,0 +1,22 @@
+//
+//  Filme.swift
+//  Meus filmes teste
+//
+//  Created by Luis Lima on 13/06/2019
+//  Copyright © 2019 Andr3ziim All rights reserved.
+//
+
+import UIKit
+
+class Filme{
+    var titulo: String!
+    var descricao: String!
+    var imagem: UIImage!
+    
+    init(titulo: String, descricao: String, imagem: UIImage) {
+        self.titulo = titulo
+        self.descricao = descricao
+        self.imagem = imagem
+    }
+    
+}
